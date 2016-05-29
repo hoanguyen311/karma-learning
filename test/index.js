6d76ddc77fd3c60254784f211471ad1c12ae8654
Hoa Nguyen Thai <hoa.nguyen-thai@lazada.com>
2016-05-29 14:38:22 +0700

    initial project

---
+++ test/index.js
@@ -0,0 +1,27 @@
+describe('Array', function  () {
+    var arr;
+    before(function() {
+        arr = [ 2, 4, 0 ];
+    });
+
+    it('Shoul be an array', function() {
+        expect(arr).to.be.an.instanceof(Array);
+    });
+
+    it('Should contain more than 3 elements', function() {
+        expect(arr).to.have.length.above(3);
+    });
+
+    it('Should include 2', function() {
+        expect(arr).include(2);
+    });
+
+    it('should take less than 500ms', function(done) {
+        this.timeout(500);
+        setTimeout(done, 400);
+    });
+
+    it('Last element should be 0', function() {
+        expect(arr[arr.length - 1]).to.equal(0);
+    });
+});
